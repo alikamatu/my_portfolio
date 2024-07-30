78f78254cf0729e51a1a72f7e8243ad497fb86f4
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-07-30 03:11:46 +0000

    Add files via upload

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -4,41 +4,30 @@ export const project = [
     {
         id: 1,
         imageUrl: photo,
-        name: 'Project Name',
-        stack: 'Mongo Express React Node Sass HTML5',
-        description: 'Highly motivated project',
-        category: 'web'
+        name: 'Apple iPhone 15',
+        stack: 'React.js CSS5 HTML5 GSAP Three.js',
+        description: 'Inspired by the cool scrolling and animation of Apple iPhone 15 website and decided to clone it!',
+        category: 'web',
+        url: "https://iphone-eta-three.vercel.app/"
     },
     {
         id: 2,
         imageUrl: photo,
-        name: 'Project Name',
+        name: 'Food Delivery',
         stack: 'Mongo Express React Node Sass HTML5',
-        description: 'Highly motivated project',
-        category: 'web'
+        description: 'A website where users can order for some set of food with payment method, full functioning',
+        category: 'web',
+        url: 'https://food-develivery.onrender.com/'
     },
     {
         id: 3,
         imageUrl: photo,
-        name: 'Project Name',
-        description: 'Highly motivated project',
-        category: 'photo'
-    },
-    {
-        id: 4,
-        imageUrl: photo,
-        name: 'Project Name',
-        description: 'Highly motivated project',
-        category: 'photo',
-    },
-    {
-        id: 5,
-        imageUrl: photo,
-        name: 'Project Name',
+        name: 'Food Delivery Admin',
         stack: 'Mongo Express React Node Sass HTML5',
-        description: 'Highly motivated project',
-        category: 'web'
-    }
+        description: 'Add, Remove and modifies orders on the admin panel',
+        category: 'web',
+        url: 'https://food-develivery-admin.onrender.com'
+    },
 ]
 
 export const catlist = [
