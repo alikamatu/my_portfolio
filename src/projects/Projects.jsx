e5b451634dad4f7cc42ae2aa6ffbf4a11f9c0bbb
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-07-30 03:05:11 +0000

    Add files via upload

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -3,21 +3,23 @@ import "./Projects.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
 import { project } from '../assets/projects'
+import { Link } from 'react-router-dom'
 
 export const Projects = ({menu}) => {
   return (
     <div className='projects'>
+      
       {project.map((item, index) => {
         if (menu==="All" || menu===item.category) {
         return  <div className="project-template" key={index}>
-        <div className="image-sec">
-        <img src={item.imageUrl} alt="" />
+        <div className="image-sec" style={{overflow: 'hidden'}}>
+        <iframe className='img' src={item.url} alt="" />
         </div>
         <div className="details-sec">
           <div className="top">
           <h2>{item.name}</h2>
           <hr />
-          <FontAwesomeIcon icon={faLink} className='icon' /> 
+         <Link to={item.url} target='_blank' ><FontAwesomeIcon icon={faLink} className='icon' /> </Link>
           </div>
           <div className="stack">
             <p>{item.stack}</p>
